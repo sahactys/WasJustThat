58ca70360d2cf9d7b34072d3d61e3a578f55ec90
sahactys <895004562613ds@gmail.com>
2022-03-16 16:23:29 +0300

    add gitinit and changing variable names

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 const val DAY = 86400
 const val HOUR = 3600
-const val MIN = 60
+const val MINUTE = 60
 
 fun main() {
     while (true) {
@@ -21,8 +21,8 @@ fun main() {
 
 fun agoToText(time: Int) {
     when (time) {
-        in 0..MIN -> println("Был(а) только что")
-        in MIN + 1..HOUR -> selectWordEnding(time)
+        in 0..MINUTE -> println("Был(а) только что")
+        in MINUTE + 1..HOUR -> selectWordEnding(time)
         in HOUR + 1..DAY -> selectWordEnding(time)
         in DAY + 1..DAY * 2 -> println("Был(а) сегодня")
         in DAY * 2 + 1..DAY * 3 -> println("Был(а) вчера")
@@ -33,10 +33,10 @@ fun agoToText(time: Int) {
 
 fun selectWordEnding(time: Int) {
     when (time) {
-        in MIN + 1..HOUR -> {
-            if (time / MIN % 10 == 1 && time / MIN != 11) println("Был(а) " + time / MIN + " минуту назад")
-            else if (time / MIN % 10 in 2..4 && time / MIN !in 12..14) println("Был(а) " + time / MIN + " минуты назад")
-            else println("Был(а) " + time / MIN + " минут назад")
+        in MINUTE + 1..HOUR -> {
+            if (time / MINUTE % 10 == 1 && time / MINUTE != 11) println("Был(а) " + time / MINUTE + " минуту назад")
+            else if (time / MINUTE % 10 in 2..4 && time / MINUTE !in 12..14) println("Был(а) " + time / MINUTE + " минуты назад")
+            else println("Был(а) " + time / MINUTE + " минут назад")
         }
         in HOUR + 1..DAY -> {
             if (time / HOUR % 10 == 1 && time / HOUR != 11) println("Был(а) " + time / HOUR + " час назад")
