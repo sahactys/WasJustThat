72f87752f269ae1e50a62d6b9a2c2b0045829ce0
Сахацкий Дмитрий <895004562613ds@gmail.com>
2022-03-30 08:47:57 +0300

    refactoring

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -24,8 +24,8 @@ fun main() {
 fun agoToText(time: Int): String {
     return when (time) {
         in 0..MINUTE -> ("Был(а) только что")
-        in MINUTE + 1..HOUR -> selectWordEnding(time)
-        in HOUR + 1..DAY -> selectWordEnding(time)
+        in MINUTE + 1..HOUR -> selectWordEnding(time, MINUTE, "минуту", "минуты", "минут")
+        in HOUR + 1..DAY -> selectWordEnding(time, HOUR, "час", "часа", "часов")
         in DAY + 1..DAY * 2 -> ("Был(а) сегодня")
         in DAY * 2 + 1..DAY * 3 -> ("Был(а) вчера")
         in DAY * 3 + 1..Int.MAX_VALUE -> ("Был(а) давно")
@@ -33,23 +33,10 @@ fun agoToText(time: Int): String {
     }
 }
 
-fun selectWordEnding(time: Int): String {
-    val timeMinute = time / MINUTE
-    val timeHour = time / HOUR
-    return when (time) {
-        in MINUTE + 1..HOUR -> {
-            if (timeMinute % 10 == 1 && timeMinute != 11) ("Был(а) $timeMinute минуту назад")
-            else if (timeMinute % 10 in 2..4 && timeMinute !in 12..14) ("Был(а) $timeMinute минуты назад")
-            else ("Был(а) $timeMinute минут назад")
-        }
-        in HOUR + 1..DAY -> {
-            if (timeHour % 10 == 1 && timeHour != 11) ("Был(а) $timeHour час назад")
-            else if (timeHour % 10 in 2..4 && timeHour !in 12..14) ("Был(а) $timeHour часа назад")
-            else ("Был(а) $timeHour часов назад")
-        }
-        else -> error("Что-то пошло не так")
-
-
-    }
+fun selectWordEnding(time: Int, divider: Int, pluralOne: String, pluralFew: String, pluralMany: String): String {
+    val timeValue = time / divider
+    return if (timeValue % 10 == 1 && timeValue != 11) ("Был(а) $timeValue $pluralOne назад")
+    else if (timeValue % 10 in 2..4 && timeValue !in 12..14) ("Был(а) $timeValue $pluralFew назад")
+    else ("Был(а) $timeValue $pluralMany назад")
 }
 
